d5096f4a52f3411606846f3456295e79d9ff0c2f
Milt Reder <milt@yetanalytics.com>
2021-08-19 12:13:47 -0400

    relative links to images work on both server/file

--- src/com/yetanalytics/lrs_admin_ui/views/login.cljs
+++ src/com/yetanalytics/lrs_admin_ui/views/login.cljs
@@ -10,7 +10,7 @@
    [:div {:class "main-wrapper"}
     [:div {:class "logo-image"}
      [:i
-      [:img {:src "/images/logo.png", :alt "LRS Logo"}]]]
+      [:img {:src "images/logo.png", :alt "LRS Logo"}]]]
     [:h1 "Login"]
     [:div {:class "form-wrapper"}
      [:form {:id "login-form"}
