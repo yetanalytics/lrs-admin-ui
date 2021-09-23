b5e749a2affc78bdf777269d667abc79c8409f1a
kelvinqian00 <kelvinqian2@gmail.com>
2021-09-23 14:41:50 -0400

    Improve password generator

--- src/com/yetanalytics/lrs_admin_ui/functions.cljs
+++ src/com/yetanalytics/lrs_admin_ui/functions.cljs
@@ -25,12 +25,32 @@
     text
     (str (subs text 0 (- len 3)) "...")))
 
+;; Character classes
+(def digit-chars (map char (range 48 58)))  ; 0-9
+(def upper-chars (map char (range 65 91)))  ; A-Z
+(def lower-chars (map char (range 97 123))) ; a-z
+(def special-chars (concat (map char (range 33 48))   ; !"#$%&'()*+,-./
+                           (map char (range 58 65))   ; :;<=>?@
+                           (map char (range 91 97))   ; [\]^_`
+                           (map char (range 123 127)) ; {|}~
+                           ))
+
 (defn pass-gen
-  "Given length generate a password with random letters numbers and specials"
+  "Given length `n`, generate a password with random uppercase and lowercase
+   letters, numbers, and specials. At least one of each character category will
+   be included in the generated password. Note that `n` should be at at least
+   4 or else undefined behavior will occur."
   [n]
-  (let [chars (map char (concat (range 48 57)
-                                (range 97 122)
-                                (range 65 90)
-                                [\! \? \#]))
-        password (take n (repeatedly #(rand-nth chars)))]
-    (reduce str password)))
+  (let [;; Password
+        dcount (inc (rand-int (- n 3)))
+        ucount (inc (rand-int (- n 2 dcount)))
+        lcount (inc (rand-int (- n 1 dcount ucount)))
+        scount (- n dcount ucount lcount)
+        ;; Password chars
+        nseq (take dcount (repeatedly (partial rand-nth digit-chars)))
+        useq (take ucount (repeatedly (partial rand-nth upper-chars)))
+        lseq (take lcount (repeatedly (partial rand-nth lower-chars)))
+        sseq (take scount (repeatedly (partial rand-nth special-chars)))]
+    (->> (concat nseq useq lseq sseq)
+         shuffle
+         (reduce str))))
