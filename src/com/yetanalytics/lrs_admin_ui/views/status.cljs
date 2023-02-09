8843444d7641519d018b218b32b58d75b52e92a9
Milt Reder <milt@yetanalytics.com>
2023-02-09 11:29:34 -0500

    SQL-172 big number styles

--- src/com/yetanalytics/lrs_admin_ui/views/status.cljs
+++ src/com/yetanalytics/lrs_admin_ui/views/status.cljs
@@ -5,10 +5,10 @@
 (defn big-number
   [label sub-qvec]
   [:div.big-number
-   [:div.big-number-label
-    label]
    [:div.big-number-value
-    @(subscribe sub-qvec)]])
+    @(subscribe sub-qvec)]
+   [:div.big-number-label
+    label]])
 
 (defn refresh-button
   []
@@ -23,4 +23,5 @@
    [:h2 {:class "content-title"}
     "LRS Status"]
    [refresh-button]
-   [big-number "Statement Count" [:status.data/statement-count]]])
+
+   [big-number "STATEMENTS" [:status.data/statement-count]]])
