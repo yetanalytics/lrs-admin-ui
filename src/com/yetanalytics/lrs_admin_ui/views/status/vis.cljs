7c3bfcefb7f16c74b02ee9c1e25ee05769a393d3
Milt Reder <milt@yetanalytics.com>
2023-02-14 15:25:13 -0500

    SQL-172 add NPM victory

--- src/com/yetanalytics/lrs_admin_ui/views/status/vis.cljs
+++ src/com/yetanalytics/lrs_admin_ui/views/status/vis.cljs
@@ -1,7 +1,6 @@
 (ns com.yetanalytics.lrs-admin-ui.views.status.vis
   (:require [reagent.core :as r]
-            [cljsjs.victory]))
+            [victory :refer [VictoryPie]]))
 
-;; TODO: This cannot use cljsjs. We need to get it working like OIDC client does for adv comp.
 (def pie
-  (r/adapt-react-class js/Victory.VictoryPie))
+  (r/adapt-react-class VictoryPie))
