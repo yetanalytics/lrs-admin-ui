a92fd702b216da8dd8998461e059387ee896d565
Cliff Casey <cliff@yetanalytics.com>
2024-09-16 20:02:41 -0400

    bootstrap 5 modifications to maintain design. included a shim for now for a couple deprecated styles

--- src/com/yetanalytics/lrs_admin_ui/views/update_password.cljs
+++ src/com/yetanalytics/lrs_admin_ui/views/update_password.cljs
@@ -15,39 +15,41 @@
       (let [new-password @(subscribe [:update-password/new-password])]
         [:div {:class "update-password-inputs"}
          [:div {:class "row"}
-          [:label {:for "old-password-input"} 
-           @(subscribe [:lang/get :account-mgmt.update-password.password.old])]
-          [:input {:value @(subscribe [:update-password/old-password])
-                   :class "new-password round"
-                   :id "old-password-input"
-                   :type "password"
-                   :on-change #(dispatch [:update-password/set-old-password
-                                          (fns/ps-event-val %)])}]]
+          [:div 
+           [:label {:for "old-password-input"} 
+            @(subscribe [:lang/get :account-mgmt.update-password.password.old])]
+           [:input {:value @(subscribe [:update-password/old-password])
+                    :class "new-password round"
+                    :id "old-password-input"
+                    :type "password"
+                    :on-change #(dispatch [:update-password/set-old-password
+                                           (fns/ps-event-val %)])}]]]
          [:div {:class "row pt-2"}
-          [:label {:for "new-password-input"}
-           @(subscribe [:lang/get :account-mgmt.update-password.password.new])]
-          [:input (cond-> {:value new-password
-                           :class "new-password round"
-                           :id "new-password-input"
-                           :on-change #(dispatch [:update-password/set-new-password
-                                                  (fns/ps-event-val %)])}
-                    @hide-pass (merge {:type "password"}))]
-          [:ul {:class "action-icon-list"}
-           [:li
-            [:a {:href "#!",
-                 :class "icon-secret pointer"
-                 :on-click #(swap! hide-pass not)}
-             (str (cond
-                    @hide-pass @(subscribe [:lang/get :account-mgmt.update-password.password.show])
-                    :else @(subscribe [:lang/get :account-mgmt.update-password.password.hide])))]]
-           [:li
-            [copy-text
-             {:text new-password
-              :on-copy #(dispatch [:notification/notify false
-                                   @(subscribe 
-                                     [:lang/get
-                                      :notification.account-mgmt.copied-password])])}
-             [:a {:class "icon-copy pointer"} @(subscribe [:lang/get :account-mgmt.update-password.password.copy])]]]]]
+          [:div
+           [:label {:for "new-password-input"}
+            @(subscribe [:lang/get :account-mgmt.update-password.password.new])]
+           [:input (cond-> {:value new-password
+                            :class "new-password round"
+                            :id "new-password-input"
+                            :on-change #(dispatch [:update-password/set-new-password
+                                                   (fns/ps-event-val %)])}
+                     @hide-pass (merge {:type "password"}))]
+           [:ul {:class "action-icon-list"}
+            [:li
+             [:a {:href "#!",
+                  :class "icon-secret pointer"
+                  :on-click #(swap! hide-pass not)}
+              (str (cond
+                     @hide-pass @(subscribe [:lang/get :account-mgmt.update-password.password.show])
+                     :else @(subscribe [:lang/get :account-mgmt.update-password.password.hide])))]]
+            [:li
+             [copy-text
+              {:text new-password
+               :on-copy #(dispatch [:notification/notify false
+                                    @(subscribe 
+                                      [:lang/get
+                                       :notification.account-mgmt.copied-password])])}
+              [:a {:class "icon-copy pointer"} @(subscribe [:lang/get :account-mgmt.update-password.password.copy])]]]]]]
          [:span {:class "password-note"}
           (format @(subscribe [:lang/get :account-mgmt.update-password.guidelines])
                   p-min-len
