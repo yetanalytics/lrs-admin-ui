c45b8cf810879da1f42bf2245744df0ff969c232
kelvinqian00 <kelvinqian2@gmail.com>
2025-02-07 13:55:23 -0500

    Add CSV Download button to Data Management page

--- src/com/yetanalytics/lrs_admin_ui/views/data_management.cljs
+++ src/com/yetanalytics/lrs_admin_ui/views/data_management.cljs
@@ -1,9 +1,18 @@
 (ns com.yetanalytics.lrs-admin-ui.views.data-management
-  (:require [re-frame.core :refer [subscribe]]
+  (:require [re-frame.core :refer [dispatch subscribe]]
             [com.yetanalytics.lrs-admin-ui.views.data-management.delete-actor :refer [delete-actor]]))
 
 (defn data-management []
-      [:div {:class "left-content-wrapper"}
-       [:h2 {:class "content-title"}
-        @(subscribe [:lang/get :datamgmt.title])]
-       [delete-actor]])
+  [:div {:class "left-content-wrapper"}
+   ;; Delete Actor
+   [:h2 {:class "content-title"}
+    @(subscribe [:lang/get :datamgmt.title])]
+   [delete-actor]
+   [:div {:class "h-divider"}]
+   ;; Download CSV
+   [:h4 {:class "content-title"}
+    @(subscribe [:lang/get :datamgmt.download.title])]
+   [:input {:type "button",
+            :class "btn-brand-bold",
+            :on-click #(dispatch [:notification/notify false "TODO: Implement CSV Download"])
+            :value @(subscribe [:lang/get :datamgmt.download.button])}]])
